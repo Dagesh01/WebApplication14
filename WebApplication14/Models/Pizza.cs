1c27ba8250fd38fe604ae2019d63bda69b6e10ea
Dagesh01 <88884634+Dagesh01@users.noreply.github.com>
2021-12-17 17:16:03 +0500

    Update Pizza.cs

--- WebApplication14/Models/Pizza.cs
+++ WebApplication14/Models/Pizza.cs
@@ -7,12 +7,15 @@ namespace WebApplication14.Models
 {
     public class Pizza
     {
+        [Required]
         public Guid Id { get; set; }
-        
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string ShortName { get; set; }
+        [Required]
         public string StockStatus { get; set; }
-
+        [Required]
         public string Ingredients { get; set; }
         public double Price { get; set; }
     }
